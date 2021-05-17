59f07234f9ceab235db5a04baaed9ae7e558dc80
licc <961150665@qq.com>
2021-05-17 12:50:03 +0800

    new: 生成push.sh自动提交代码[

---
+++ push.sh
@@ -0,0 +1,15 @@
+# 先输入commit信息，确认后再暂存代码
+read -p "→ 请输入您的commit提交信息：" MSG
+
+echo → 暂存选取所有代码
+git add .
+
+echo → 提交所有暂存代码
+git commit -m "$MSG"
+
+echo → 将代码推送至远端git仓库
+git push -u orign 2.6
+
+echo → 重新fetch最新代码
+git fetch
+
